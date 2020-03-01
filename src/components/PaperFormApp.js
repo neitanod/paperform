70f586c56b9b14a061bb62c362bd5c9356df18a7
Sebastián Grignoli <grignoli@gmail.com>
2020-03-01 10:32:31 -0300

    Rename App.js to PaperFormApp.js

--- src/components/PaperFormApp.js
+++ src/components/PaperFormApp.js
@@ -1,6 +1,17 @@
+import functions from './functions.js';
+const readFile = functions.readFile;
+const addCssFile = functions.addCssFile;
+
+import NumberRow from './NumberRow.js';
+
+addCssFile('./components/PaperFormApp.css');
+
 export default {
     name: 'App',
-    template: document.getElementById("paperform-app").outerHTML,
+    template: document.getElementById("paperform-app-template").outerHTML,
+    components: {
+        'number-row': NumberRow
+    },
     data: function() {
         return {
             greeting: "Hi there!",
