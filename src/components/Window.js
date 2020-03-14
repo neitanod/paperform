7da4ed6b47175b9bca2563ac6d344a338a0c21b4
Sebastián Grignoli <grignoli@gmail.com>
2020-03-14 17:20:33 -0300

    Las ventanas se pueden posicionar y estar fixed (no scrollear con la pagina)

--- src/components/Window.js
+++ src/components/Window.js
@@ -8,11 +8,28 @@ export default async function() {
     return {
         name: 'Window',
         template: await readFile('components/Window.html'),
+        props: {
+            'top':    { default: null },
+            'left':   { default: null },
+            'bottom': { default: null },
+            'right':  { default: null },
+        },
         data: function() {
             return {
             }
         },
+        mounted: function() {
+            this.$el.style.top = this.top;
+            this.$el.style.left = this.left;
+            this.$el.style.bottom = this.bottom;
+            this.$el.style.right = this.right;
+        },
         methods: {
+            dragged: function(x,y) {
+                console.log("Dragged!");
+                console.log("X:" + x);
+                console.log("Y:" + y);
+            }
         }
     }
 }
