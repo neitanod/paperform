970942bee89112d9b6d0690f9eaf766ad27322b4
Sebastián Grignoli <grignoli@gmail.com>
2020-03-25 03:31:32 -0300

    Hago moveable el elemento padre

--- src/components/Element.js
+++ src/components/Element.js
@@ -16,12 +16,26 @@ export default async function() {
             'pa-element-text':   ElementText,
             'pa-element-box':    ElementBox,
             'pa-element-image':  ElementImage,
+            'moveable':          VueMoveable
         },
         props: {
             element: { default: {} }
         },
         data: function() {
             return {
+                moveable: {
+                    draggable: true,
+                    throttleDrag: 1,
+                    resizable: true,
+                    throttleResize: 1,
+                    keepRatio: false,
+                    scalable: (this.element.type == "image"),
+                    throttleScale: 0.01,
+                    rotatable: true,
+                    throttleRotate: 0.2,
+                    pinchable: true,
+                    origin: false
+                },
             }
         },
         mounted: function() {
@@ -29,6 +43,32 @@ export default async function() {
         beforeDestroy: function() {
         },
         methods: {
+            handleDrag({ target, transform }) {
+                console.log("onDrag", transform);
+                target.style.transform = transform;
+            },
+            handleResize({ target, width, height }) {
+                console.log("onResize", width, height);
+                target.style.width = `${width}px`;
+                target.style.height = `${height}px`;
+            },
+            handleScale({ target, transform }) {
+                console.log("onScale", transform);
+                target.style.transform = transform;
+            },
+            handleRotate({ target, transform }) {
+                console.log("onRotate", transform);
+                target.style.transform = transform;
+            },
+            handleWarp({ target, transform }) {
+                console.log("onWarp", transform);
+                target.style.transform = transform;
+            },
+            clearAllStates() {
+                Object.keys(this.states).forEach(key => {
+                    this.moveable[key] = false;
+                });
+            }
         }
     }
 }
